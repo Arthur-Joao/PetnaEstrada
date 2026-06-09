db0d6dafc7beae13fdc76dfa528734555e9a7247
Arthur-Joao <arthurjoaof@gmail.com>
2026-06-09 16:33:25 -0300

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -6,5 +6,26 @@ from django.contrib import messages
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        animais = Animal.objects.all()
-        return render(request, 'index.html', {'animais': animais})
+
+        context = {
+            'animais': Animal.objects.all(),
+            'tipos': Tipo.objects.all(),
+            'status_list': Status.objects.all(),
+            'bairros': Bairro.objects.all(),
+        }
+
+        return render(request, 'index.html', context)
+
+    # def get(self, request, *args, **kwargs):
+    #     animais = Animal.objects.all()
+    #     return render(request, 'index.html', {'animais': animais})
+    
+    # def cadastrar_animal(request):
+
+    #     context = {
+    #         'tipos': Tipo.objects.all(),
+    #         'status_list': Status.objects.all(),
+    #         'bairros': Bairro.objects.all(),
+    #     }
+
+    #     return render(request, 'cadastro_animal.html', context)
